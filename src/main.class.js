205c9696cd573ac9740c980d035ea65dc19b4836
taqin <abdulmuttaqin456@gmail.com>
2023-11-24 09:02:01 +0700

    update .gitignore, main.class.js and tes.js update main.class.js and tes.js

--- src/main.class.js
+++ src/main.class.js
@@ -3,13 +3,27 @@ const { BASE_URL ,PANAMERA_CLIENT} = require('./constant');
 const { generateDeviceInfo } = require('./utils/genDeviceInfo');
 const cookieBuilder = require('./utils/cookieBuilder');
 const { load } = require('cheerio');
+
 class OLXClient {
+
+     /**
+     * Create an OLX client.
+     * Author: fdciabdul
+     * @param {string} username - The username for OLX.
+     * @param {string} password - The password for OLX.
+     */
     constructor(username,password) {
         this.user = username;
         this.password = password;
         this.baseUrl = BASE_URL;
     }
 
+    /**
+     * Build headers for HTTP requests.
+     * Author: fdciabdul
+     * @param {string} [referrer=this.baseUrl] - The referrer URL.
+     * @returns {Object} The headers object.
+     */
     buildHeaders(referrer = this.baseUrl) {
         return {
             "accept": "*/*",
@@ -27,8 +41,15 @@ class OLXClient {
         };
     }
 
+    /**
+     * Log in to OLX.
+     * Author: fdciabdul
+     * @returns {Promise<Object>} A promise that resolves to the login response.
+     * @throws {Error} Throws an error if login fails.
+     */
     async loginOLX() {
         try {
+            console.log(this.user,this.password)
             const response = await axios.post(`${this.baseUrl}/api/auth/authenticate/login`, {
                 grantType: "email",
                 email : this.user,
@@ -40,12 +61,24 @@ class OLXClient {
             });
             const { accessToken, refreshToken, user, chatToken, notificationHubId } = response.data;
             const cookie = cookieBuilder(user,accessToken,refreshToken,chatToken,notificationHubId);
-            return cookie;
+            return {
+                statu : true,
+                message : "Login Success",
+                cookie : cookie
+            };
         } catch (error) {
             throw new Error('Login Failed: ' + error.message);
         }
     }
 
+    /**
+     * Fetch user data from OLX.
+     * Author: fdciabdul
+     * @param {string} userId - The user ID.
+     * @param {string} cookie - The cookie obtained after login.
+     * @returns {Promise<Object>} A promise that resolves to user data.
+     * @throws {Error} Throws an error if fetching user data fails.
+     */
     async getUserData(userId, cookie) {
         try {
             const response = await axios.get(`${this.baseUrl}/api/users/${userId}`, {
@@ -61,8 +94,13 @@ class OLXClient {
         }
     }
 
+    /**
+     * Get categories from OLX.
+     * Author: fdciabdul
+     * @returns {Promise<Array>} A promise that resolves to an array of category links.
+     */
     async getCategory(){
-        const response = await axios.get(BASE_URL);
+        const response = await axios.get(this.baseUrl);
         
         const $ = load(response.data);
         const links = [];
@@ -80,6 +118,61 @@ class OLXClient {
         });
 
         return links;
+    }
+
+    /**
+     * Search for categories on OLX.
+     * Author: fdciabdul
+     * @param {number} categoryId - The ID of the category to search.
+     * @param {number} locationId - The ID of the location to search in.
+     * @returns {Promise<Object>} A promise that resolves to the search results.
+     * @throws {Error} Throws an error if search fails.
+     */
+    async searchCategory(categoryId, locationId) {
+        const params = {
+            category: categoryId,
+            facet_limit: 100,
+            location: locationId,
+            location_facet_limit: 20,
+            platform: 'web-desktop',
+            relaxedFilters: true
+        };
+
+        try {
+            const response = await axios.get(`${this.baseUrl}/api/relevance/v4/search`, { params });
+            return response.data;
+        } catch (error) {
+            console.error('Error in searchCategory:', error);
+            throw error; 
+        }
+    }
+
+     /**
+     * Search for locations on OLX.
+     * Author: fdciabdul
+     * @param {string} query - The search query for location.
+     * @returns {Promise<Object>} A promise that resolves to location search results.
+     * @throws {Error} Throws an error if location search fails.
+     */
+    async searchLocation(query){
+        const params = {
+            input: query,
+            limit: 5,
+        };
+
+        try {
+            const {data} = await axios.get(`${this.baseUrl}/api/locations/autocomplete`, { params });
+            if(data){
+                return {
+                    status : true,
+                    location : data.data.suggestions
+                };
+            }
+            
+        } catch (error) {
+            console.error('Error in searchCategory:', error);
+            throw error; 
+        }
     }
 }
 
--- tes.js
+++ tes.js
@@ -1,11 +1,14 @@
 const { OLXClient } = require("./src/main.class");
 
 (async () => {
-    const client = new OLXClient();
+    const client = new OLXClient('abdulmuttaqin456@gmail.com', 'mana123');
     try {
+        const loginData = await client.searchLocation("Jakarta Selatan");
 
-        const userData = await client.getCategory();
-        console.log(userData);
+     
+        
+      //  const userData = await client.getUserData('100042848', cookie);
+        console.log(loginData);
     } catch (error) {
         console.error(error.message);
     }
